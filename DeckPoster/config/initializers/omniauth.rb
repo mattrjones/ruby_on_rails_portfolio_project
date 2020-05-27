306ca2de8beb67e4ccd8d4bad713919583fc23dd
Matthew Jones <matts3371@gmail.com>
2020-05-26 23:01:56 -0500

    Added omniauth support for github

--- DeckPoster/config/initializers/omniauth.rb
+++ DeckPoster/config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
-  end
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+end
